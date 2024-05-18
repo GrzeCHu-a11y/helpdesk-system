7d52d8e284d8e3fbaad5e457b5bdb1fa3eaea33a
GrzeCHu-a11y <grzegorzgonczowski9@gmail.com>
2024-05-18 20:40:07 +0200

    created database config

---
+++ db/dbconfig.php
@@ -0,0 +1,8 @@
+<?php
+class Config
+{
+    public static $servername = "localhost";
+    public static $username = "admin";
+    public static $password = "admin";
+    public static $database = "helpdesk_system";
+}
